41dc9f0e062767044464fc3737200464515f46b5
Adam Milligan <adam@srp-ok.com>
2020-05-11 10:27:56 -0500

    Prevent error when Elasticsearch is not required
    Authored-by: Adam Milligan <adam@srp-ok.com>

--- lib/rspec/common/extensions/elasticsearch/client.rb
+++ lib/rspec/common/extensions/elasticsearch/client.rb
@@ -1,43 +1,48 @@
 # frozen_string_literal: true
 
-require "elasticsearch/model"
+# rubocop:disable Lint/SuppressedException
+begin
+  require "elasticsearch/model"
 
-module Doubles
-  module Elasticsearch
-    class Client
-      class << self
-        def reset!
-          @calls = nil
+  module Doubles
+    module Elasticsearch
+      class Client
+        class << self
+          def reset!
+            @calls = nil
+          end
+
+          def calls
+            @calls ||= {}
+          end
         end
 
-        def calls
-          @calls ||= {}
+        def initialize(*args); end
+
+        def index(**params)
+          self.class.calls[:create] ||= []
+          self.class.calls[:create] << params
         end
-      end
 
-      def initialize(*args); end
+        def update(**params)
+          self.class.calls[:update] ||= []
+          self.class.calls[:update] << params
+        end
 
-      def index(**params)
-        self.class.calls[:create] ||= []
-        self.class.calls[:create] << params
-      end
-
-      def update(**params)
-        self.class.calls[:update] ||= []
-        self.class.calls[:update] << params
-      end
-
-      def delete(**params)
-        self.class.calls[:delete] ||= []
-        self.class.calls[:delete] << params
+        def delete(**params)
+          self.class.calls[:delete] ||= []
+          self.class.calls[:delete] << params
+        end
       end
     end
   end
-end
 
-Elasticsearch::Client.class_eval do
-  def new(*)
-    Doubles::Elasticsearch::Client.new
+  Elasticsearch::Client.class_eval do
+    def new(*)
+      Doubles::Elasticsearch::Client.new
+    end
   end
+rescue LoadError
 end
+# rubocop:enable Lint/SuppressedException
 
