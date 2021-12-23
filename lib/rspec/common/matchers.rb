e02bcccd964fe5e74ad0229eec7407450dc4f7f8
Adam Milligan <adam@buildgroundwork.com>
2021-12-23 14:03:02 -0500

    Add generic matcher for side-effects
    This is mostly useful for matchers like have_received that have to be
    called after the action and are not called on the subject.  So, e.g.:
    
      it { should satisfy { expect(foo).to have_received(:wibble) } }
    
    vs.
    
      it "calls the method" do
        subject.call
        expect(foo).to have_received(:wibble)
      end
    
    Authored-by: Adam Milligan <adam@buildgroundwork.com>

--- lib/rspec/common/matchers.rb
+++ lib/rspec/common/matchers.rb
@@ -4,3 +4,15 @@ Dir.glob(File.expand_path("matchers/**/*.rb", __dir__)).each do |path|
   require path
 end
 
+RSpec::Matchers.define :satisfy do
+  match do |action|
+    action.call
+    block_arg.call
+    true
+  end
+
+  failure_message do
+    "expected action to satisfy expectation"
+  end
+end
+
