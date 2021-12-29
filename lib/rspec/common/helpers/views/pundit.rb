8c5528f4709a1b8d60b5c2e662c40ca56c2d3a09
Adam Milligan <adam@buildgroundwork.com>
2021-12-29 15:45:45 -0500

    Add helper for view specs that reference Pundit
    Authored-by: Adam Milligan <adam@buildgroundwork.com>

---
+++ lib/rspec/common/helpers/views/pundit.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# This adds Pundit to test view objects, so view tests that use Pundit methods
+# such as #policy or #pundit_user will work properly.  Useful if you are using
+# Jbuilder#authorize! from the rails-boost gem along with Pundit.
+module RSpec::Common
+  module Helpers
+    module Views
+      module Pundit
+        class << self
+          def included(klass)
+            klass.setup(:add_pundit_to_test_views)
+          end
+        end
+
+        private
+
+        def add_pundit_to_test_views
+          view.singleton_class.instance_eval { include ::Pundit }
+        end
+      end
+    end
+  end
+end
+
