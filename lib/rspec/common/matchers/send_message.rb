11d67520d7c14f3db33658ea46957d216f87979d
Adam Milligan <adam@redish.com>
2022-11-10 11:12:52 -0500

    Allow passing refinements to send_message matcher
    Authored-by: Adam Milligan <adam@redish.com>

--- lib/rspec/common/matchers/send_message.rb
+++ lib/rspec/common/matchers/send_message.rb
@@ -1,24 +1,36 @@
 # frozen_string_literal: true
 
-RSpec::Matchers.define :send_message do |message|
-  chain :to do |receiver|
-    @receiver = receiver
-  end
+MatchRefinement = Struct.new(:message, :args, :kwargs)
 
-  chain :with do |args|
-    @args = args
+RSpec::Matchers.define :send_message do |message|
+  chain :to, :receiver
+  chain :with, :args
+
+  define_method :refinements do
+    @refinements ||= []
+  end
+  private :refinements
+
+  define_method(:method_missing) do |method, *args, **kwargs|
+    if respond_to?(method)
+      super(method, *args, **kwargs)
+    else
+      refinements << MatchRefinement.new(method, args, kwargs)
+      self
+    end
   end
 
   match do |action|
-    raise "The `send_message` matcher requires a receiver" unless @receiver
+    raise "The `send_message` matcher requires a receiver" unless receiver
 
-    allow(@receiver).to receive(message)
+    allow(receiver).to receive(message)
 
     action.call
 
     match = have_received(message)
-    match.with(@args) if @args
-    expect(@receiver).to(match)
+    match = match.with(args) if args
+    match = refinements.inject(match) { |refined, ref| refined.public_send(ref.message, *ref.args, **ref.kwargs) }
+    expect(receiver).to(match)
   end
 end
 
