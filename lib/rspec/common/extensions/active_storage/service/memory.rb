96e86b999d8dc97736efac6be6fb39039e286e2d
Adam Milligan <adam@redish.com>
2023-03-05 21:56:15 -0500

    Update rubocop version
    Version 1.47 of rubocop has changes to the Lint/MissingSuper cop.  The
    new behavior no longer finds an error with the super-less initializer.

--- lib/rspec/common/extensions/active_storage/service/memory.rb
+++ lib/rspec/common/extensions/active_storage/service/memory.rb
@@ -14,9 +14,7 @@
 module ActiveStorage
   class Service::MemoryService < Service
     # We explicity do NOT want the super behavior from ActiveStorage
-    # rubocop:disable Lint/MissingSuper
     def initialize(**config); end
-    # rubocop:enable Lint/MissingSuper
 
     def reset!
       @storage = nil
